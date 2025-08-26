d58a9b83f66fe0c544326f957af53c5b27a37ae8
OBackh <ole.backhaus@gmx.de>
2025-08-26 17:24:44 +0200

    add some recent files to git

---
+++ backend/src/test/java/com/example/todobackend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() { //tests should be written in near future
+    }
+
+}
