3c26bacf3c2052d2003f2541cd31de5bf744b3b0
OBackh <ole.backhaus@gmx.de>
2025-08-28 17:17:01 +0200

    add db-connection-test in BackendApplication

--- backend/src/main/java/com/example/todobackend/BackendApplication.java
+++ backend/src/main/java/com/example/todobackend/BackendApplication.java
@@ -1,13 +1,26 @@
 package com.example.todobackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
-public class BackendApplication {
+@RequiredArgsConstructor
+public class BackendApplication implements CommandLineRunner {
+
+    private final MongoTemplate mongoTemplate; // Konstruktor-Injektion durch Lombok
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Prüfe MongoDB-Verbindung...");
+        log.info("MongoDB verbunden! DB-Name: {}", mongoTemplate.getDb().getName());
+    }
 }
